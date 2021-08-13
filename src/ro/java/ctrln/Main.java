b6159a6a25d6f0a00c7809e46b6efa66546cd9bb
alinostropel <alin.ostropel@gmail.com>
2021-08-13 14:30:34 +0300

    Modificare Main.java

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,6 +1,7 @@
 package ro.java.ctrln;
 
 public class Main {
+    // JAVA_HOME modificare
     //JAR - Java Archive
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java");
