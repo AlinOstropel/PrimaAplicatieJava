6fc560d0f0cebc22f2fdfc97709cba891004f524
Alin Ostropel <alin.ostropel@gmail.com>
2021-08-16 17:22:19 +0100

    Modificare Main.java - Stergere comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,9 +1,6 @@
 package ro.java.ctrln;
 
 public class Main {
-    // JAVA_HOME modificare
-    // JAVA_HOME modificare Win
-    // JAR - Java Archive
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java");
     }
