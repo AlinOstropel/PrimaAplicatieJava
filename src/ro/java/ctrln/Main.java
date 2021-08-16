d69dfec4f2a5ba41e25e8d12a862255318182031
Alin Ostropel <alin.ostropel@gmail.com>
2021-08-16 18:13:04 +0100

    Adaugare fisier nou pe .gitignore comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -2,6 +2,7 @@ package ro.java.ctrln;
 
 public class Main {
     public static void main(String[] args) {
+        // comentariu
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java");
     }
 }
