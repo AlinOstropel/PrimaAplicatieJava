9ebac74383df0c725ef839969a8e677aeda0e75c
Alin Ostropel <alin.ostropel@gmail.com>
2021-08-16 18:14:24 +0100

    Adaugare fisier nou pe .gitignore comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,7 +3,7 @@ package ro.java.ctrln;
 public class Main {
     // JAVA_HOME modificare
     // JAVA_HOME modificare Win
-    // JAR - Java Archive
+    // JAR - Java Archive jtdfvfvgnfytdxvknbjxcfxsxcdgvhb j
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java");
     }
