800fd0840db15299353b2514be4ecf62a14ca92e
Alin Ostropel <alin.ostropel@gmail.com>
2021-08-17 13:27:54 +0100

    Revert "Adaugare fisier nou pe .gitignore comentariu"
    This reverts commit 9ebac743

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,7 +3,7 @@ package ro.java.ctrln;
 public class Main {
     // JAVA_HOME modificare
     // JAVA_HOME modificare Win
-    // JAR - Java Archive jtdfvfvgnfytdxvknbjxcfxsxcdgvhb j
+    // JAR - Java Archive
     public static void main(String[] args) {
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java");
     }
